c9ed41fa512b4a738363430e34678da3c01fcd0c
Bog Gus <bog.sosnin@gmail.com>
2025-02-09 14:07:19 +0300

    new

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
+from database import get_variant
+from task1 import solve_task1
 from task2 import solve_task2
+from task3 import solve_task3
+from task4 import solve_task4
+
+def main():
+
+    # Выбираем вариант
+    variant_number = 1
+    variables = get_variant(variant_number)
+
+    solve_task1()
+    solve_task2(#a1, a2, a3, n1, n2, n3)
+    solve_task3()
+    solve_task4()
+    
+if __name__ == "__main__":
+    main()
